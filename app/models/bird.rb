f918c489882a8fb38fb840866f280323e8540700
Alejandro Marcano <+>
2021-11-28 02:21:36 +0100

    commit tree 9bb6598fbe1262b510868997ae809cc9984be316

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,5 @@
 class Bird < ApplicationRecord
 end
+#it makes sense to create a custom route when we can manipulate 
+ #our data in the backend so that when we make a request
+ #we don't have to send data when when incrememt our data
